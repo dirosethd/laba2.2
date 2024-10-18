a515c538b32c8105fd175a56d91dfea90cd51980
dirose <dirose@DESKTOP-PQA70RD>
2024-10-18 17:38:26 +0200

    Добавьте файлы проекта.

---
+++ laba2.2/Program.cs
@@ -0,0 +1,17 @@
+﻿try
+{
+    Console.WriteLine("Введите a:");
+    int a = int.Parse(Console.ReadLine());
+    Console.WriteLine("Введите b:");
+    int b = int.Parse(Console.ReadLine());
+    Console.WriteLine("Введите c:");
+    int c = int.Parse(Console.ReadLine());
+
+    if ((a == b) || (a == c) || (b == c)) Console.WriteLine("Треугольник р/б");
+    else Console.WriteLine("Треугольник не р/б");
+}
+catch
+{
+    Console.WriteLine("Введите правильно");
+}
+
